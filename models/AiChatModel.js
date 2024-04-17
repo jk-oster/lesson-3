86173b8216ba9b9e99556f3b60f79a732b25693c
Jakob Osterberger <98315600+jk-oster@users.noreply.github.com>
2024-04-17 17:16:51 +0000

    added models

--- models/AiChatModel.js
+++ models/AiChatModel.js
@@ -1,11 +1,8 @@
-'use strict';
+import KWM_Observable from '../core/kwm-observable.js';
 
-// Open AI Documentation: https://platform.openai.com/docs/api-reference/chat/object
-
-// OPENAI API TAGESMENÜ:
 // https://api.openai.com/v1/chat/completions - POST
-// Fetch request options example:
-// {
+// REQUEST Example:
+// 
 // method: "POST",
 // headers: {
 //     "Content-Type": "application/json",
@@ -15,32 +12,82 @@
 //     "model": "gpt-3.5-turbo",
 //     "messages": [{
 //          "role":"user",
-//          "content":"The message goes HeroTeaserComponent..."
+//          "content":"The message content goes here..."
 //      }]
 // }'
-// }
-
+// 
 // RESPONSE Example:
+// 
 // {
 //     "id": "chatcmpl-8sH70IAhiID98Th5qEWN64K4GiVzJ",
 //     "object": "chat.completion",
 //     "created": 1707946182,
 //     "model": "gpt-3.5-turbo-0613",
 //     "choices": [
-//     {
+//       {
 //         "index": 0,
 //         "message": {
-//             "role": "assistant",
-//             "content": "Hello! How can I assist you today?"
+//           "role": "assistant",
+//           "content": "Hello! How can I assist you today?"
 //         },
 //         "logprobs": null,
 //         "finish_reason": "stop"
-//     }
-// ],
+//       }
+//     ],
 //     "usage": {
-//     "prompt_tokens": 8,
-//         "completion_tokens": 9,
-//         "total_tokens": 17
-// },
+//       "prompt_tokens": 8,
+//       "completion_tokens": 9,
+//       "total_tokens": 17
+//     },
 //     "system_fingerprint": null
 // }
+
+class AiChatModel {
+
+    constructor() {
+        const savedApiKey = localStorage.openAiApiKey ?? '';
+
+        this.messages = new KWM_Observable([]);
+        this.openAiApiKey = new KWM_Observable(savedApiKey);
+
+        this.openAiApiKey.subscribe((value) => {
+            localStorage.openAiApiKey = value; // Sync the API key to local storage
+        });
+    }
+
+    setApiKey(apiKey) {
+        this.openAiApiKey.value = apiKey;
+    }
+
+    resetChat() {
+        this.messages.value = [];
+    }
+
+    async getAnswer(message) {
+        const newUserMessage = {
+            role: 'user',
+            content: message
+        }
+
+        this.messages.value = [...this.messages.value, newUserMessage];
+
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + this.openAiApiKey.value
+            },
+            body: JSON.stringify({
+                model: 'gpt-3.5-turbo',
+                messages: this.messages.value
+            })
+        }).then(res => res.json());
+
+        const assistantResponseMessage = response.choices[0].message
+
+        this.messages.value = [...this.messages.value, assistantResponseMessage];
+    }
+}
+
+// Singelton Instance of the Model - only one instance of a model is allowed
+export const aiChatModelInstance = new AiChatModel();
