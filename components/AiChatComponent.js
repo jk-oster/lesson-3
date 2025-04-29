52409380b1cbd45ea504d2badc66ed0dcc813b77
John Doe <johndoe@example.com>
2025-04-29 22:25:41 +0200

    add implementation

--- components/AiChatComponent.js
+++ components/AiChatComponent.js
@@ -26,13 +26,94 @@ export default class AiChatComponent extends KWM_Component {
     constructor() {
         super();
 
-   }
+        this.messages = aiChatModelInstance.messages;
+        this.openAiApiKey = aiChatModelInstance.openAiApiKey;
+        this.newMessage = new KWM_Observable('');
+        this.warningText = new KWM_Observable('');
+        this.newOpenAiApiKey = new KWM_Observable('');
+        this.isLoading = new KWM_Observable(false);
+        this.chatElem = null;
 
-    
+        this.registerRenderDependencies([this.messages, this.openAiApiKey, this.isLoading]);
+    }
+
+    /**
+     * Listener functions
+     */
+    async sendMessage() {
+        if (this.newMessage.value === '') {
+            this.warningText.value = 'Please enter a message';
+            return;
+        }
+
+        this.warningText.value = '';
+        this.isLoading.value = true;
+
+        try {
+            await aiChatModelInstance.getAnswer(this.newMessage.value);
+        }
+        catch (error) {
+            console.error(error);
+            this.warningText.value = 'Oh oh! Ein Fehler ist aufgetreten 😞. Bitte versuche es erneut oder schau in die Konsole für mehr Informationen.';
+        }
+        finally {
+            this.isLoading.value = false;
+        }
+        this.newMessage.value = '';
+    }
+
+    sendOnEnter(event) {
+        if(event.key === 'Enter') {
+            this.sendMessage();
+        }
+    }
+
+    setApiKey() {
+        aiChatModelInstance.setApiKey(this.newOpenAiApiKey.value);
+    }
+
+    resetChat() {
+        aiChatModelInstance.resetChat();
+    }
+
+    /**
+     * Template functions
+     */
     template() {
-        return /*html*/`
-        <h1>Chat UI goes here</h1>
-        
+        return `
+        <section id="main_content">
+            <h1>KWM Ai Chat</h1>
+
+                <div kwm-if="this.openAiApiKey.value === ''">
+                    <p>Please enter your OpenAI API key to start chatting</p>
+                    <input id="openai_key" type="text" kwm-model-value="newOpenAiApiKey" placeholder="OpenAI API Key" />
+                    <button id="set_key" kwm-listen-click="setApiKey">Set Key</button>
+                </div>
+
+                <div kwm-if="this.openAiApiKey.value" class="chat">
+                    <p kwm-if="this.messages.value.length === 0">Hi, ich bin dein AI Chat Bot 🤖. Frag mich was!</p>
+
+                    ${ this.messages.value.map((message) => `
+                            <div class="message ${message.role}">
+                                <p>${message.role === 'user' ? '👨‍💻 Du' : '🤖 AI'}: ${message.content}</p>
+                            </div>
+                        `).join('')
+                    }
+                    
+                    <div kwm-if="this.isLoading" class="message assistant loading">
+                        <p>🤖 AI: ... schreibt gerade ...</p>
+                    </div>
+                    
+                    <p id="warning_text">${this.warningText.value}</p>
+                </div>
+
+                <div kwm-if="this.openAiApiKey.value">
+                    <textarea id="new_message" type="text" kwm-listen-keydown="sendOnEnter" kwm-model-value="newMessage" placeholder="Type a message..." kwm-bind-disabled="isLoading" ></textarea>
+                    <button id="send_message" kwm-listen-click="sendMessage" kwm-bind-disabled="isLoading">📝Senden</button>
+                    <button kwm-if="this.messages.value.length !== 0" id="reset_chat" kwm-listen-click="resetChat">🗑️ Chat Löschen</button>
+                </div>
+
+        </section>
     `;
     }
 }
--- components/QuizComponent.js
+++ components/QuizComponent.js
@@ -1,7 +1,6 @@
 "use strict";
 import KWM_Component from '../core/kwm-component.js';
-import KWM_Observable from '../core/kwm-observable.js';
-
+import {obs} from '../core/kwm-observable.js';
 import { quizModelInstance } from '../models/QuizModel.js';
 
 /**
@@ -15,12 +14,113 @@ export default class QuizComponent extends KWM_Component {
     constructor() {
         super();
 
+        this.categories = quizModelInstance.categories;
+        this.questions = quizModelInstance.questions;
+
+        quizModelInstance.fetchCategories();
+        
+        this.numberOfQuestions = obs(10);
+        
+        this.answers = obs([]);
+        this.currentQuestion = obs(null);
+        
+        this.displaySolution = obs(false);
+        this.isLoading = obs(false);
+
+        this.registerRenderDependencies([this.categories, this.questions, this.currentQuestion, this.displaySolution, this.isLoading]);
     }
 
-    template() {
-        return /*html*/`
-        <h1>Quiz UI goes here</h1>
+    nextQuestion() {
+        const nextQuestionIndex = this.questions.value.findIndex(q => q.id === this.currentQuestion.value.id) + 1;
+        if (nextQuestionIndex >= this.questions.value.length) {
+            this.displaySolution.value = true;
+            this.currentQuestion.value = null;
+            return;
+        }
+        this.currentQuestion.value = this.questions.value[nextQuestionIndex];
+    }
+
+    /**
+     * Listener functions
+     */
+
+    async startQuiz(category) {
+        this.answers.value = []; // Reset answers
+        this.displaySolution.value = false; // Hide Solution Summary
         
+        this.isLoading.value = true;
+        
+        try {
+            await quizModelInstance.fetchQuestions(category, this.numberOfQuestions.value);
+            this.currentQuestion.value = this.questions.value[0]; // Make first question active
+        } catch(e) {
+            console.log(e);
+        } finally {
+            this.isLoading.value = false;
+        }
+    }
+
+    reset() {
+        this.answers.value = [];
+        this.displaySolution.value = false;
+    }
+
+    answerQuestion(answer) {
+        const question = this.currentQuestion.value;
+        question.correct = question.answers.find(a => a.answer === answer).is_correct;
+        question.answer = answer;
+        this.answers.value = [...this.answers.value, question];
+        this.nextQuestion();
+    }
+
+    /**
+     * Template function
+     */
+
+    template() {
+        return `
+        <section id="main_content">
+            <h1>Quiz</h1>
+
+                <div kwm-if="this.currentQuestion.value">
+                    <p><b>Category:</b> ${this.currentQuestion.value?.topic}</p>
+                    <p>${this.answers.value.length + 1} of ${this.questions.value?.length}</p>
+                    <p><i>${this.currentQuestion.value?.question}</i></p>
+                    <button kwm-listen-click="this.answerQuestion('True')">✅True</button>
+                    <button kwm-listen-click="this.answerQuestion('False')">❌False</button>
+                </div>
+
+                <div kwm-if="!this.currentQuestion.value" kwm-debug>
+                    <label for="numQuestions">Number of questions:</label>
+                    <input id="numQuestions" type="number" $value="numberOfQuestions" max="25" min="1" />
+                    
+                    <h2>To start choose a category</h2>
+                    <div class="category-gallery">
+                    ${this.categories.value.length === 0 ? `
+                            <p>Loading...</p>
+                        ` : this.categories.value.map(category => /*html*/`
+                            <button kwm-listen-click="this.startQuiz('${category}')">🏷️${category}</button>
+                        `).join('')
+                    }
+                    </div>
+
+                    <p kwm-if="this.isLoading.value">Loading...</p>
+                </div>
+
+                <div kwm-if="this.displaySolution.value">
+                    <h2>Solution</h2>
+                    <p>${this.answers.value.filter(answer => answer.correct).length} correct out of ${this.answers.value.length}</p>
+                    
+                    ${this.answers.value.map(answer => /*html*/`
+                        <div class="${answer.correct ? 'correct' : 'wrong'}">
+                            <b><i>${answer.question}</i></b>
+                            <p>Your answer: ${answer.answer}, Solution: ${answer.correct ? '✅' : '❌'}</p>
+                        </div>
+                    `).join('')}
+
+                    <button kwm-listen-click="reset">Reset</button>
+                </div>
+        </section>
     `;
     }
 }
